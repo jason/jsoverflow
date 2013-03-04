f8dc6894e72afa8847fbf72f33f8cbfba8e4be9a
Jason Wong <technique@gmail.com>
2013-03-04 14:44:28 -0800

    Added Send Grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,17 @@ Jsoverflow::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'APP_NAME_GOES_HERE.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
 
   # Enable threaded mode
   # config.threadsafe!
